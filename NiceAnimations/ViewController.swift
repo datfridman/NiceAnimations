b6a39c2c0cac8986e72ff428dc83d8e72babcdc8
Leo Fridman <madfridman@gmail.com>
2022-09-12 22:47:20 +0200

    view, button, labels

--- NiceAnimations/ViewController.swift
+++ NiceAnimations/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var presetLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var delayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
