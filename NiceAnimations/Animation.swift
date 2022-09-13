63a4793654eacdbdf2a4dacefdd3f5e3e772a4ea
Leo Fridman <madfridman@gmail.com>
2022-09-13 13:15:44 +0200

    final version

---
+++ NiceAnimations/Animation.swift
@@ -0,0 +1,16 @@
+//
+//  Animation.swift
+//  NiceAnimations
+//
+//  Created by Leon Fridman on 9/13/22.
+//
+
+import Foundation
+
+struct Animation {
+    let animation: String
+    let curves: String
+    let force: Double
+    let duration: Double
+    let delay: Double
+}
--- NiceAnimations/ViewController.swift
+++ NiceAnimations/ViewController.swift
@@ -10,21 +10,51 @@ import SpringAnimation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var animationView: SpringView!
     @IBOutlet var presetLabel: UILabel!
     @IBOutlet var curveLabel: UILabel!
     @IBOutlet var forceLabel: UILabel!
     @IBOutlet var durationLabel: UILabel!
     @IBOutlet var delayLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    @IBAction func startAnimationButtonPressed(_ sender: SpringButton) {
+        
+        
+        animationView.animation = presetLabel.text ?? "pop"
+        animationView.curve = curveLabel.text ?? "ease"
+        animationView.force = Double(forceLabel.text!) ?? 1.0
+        animationView.duration = Double(durationLabel.text!) ?? 1.5
+        animationView.delay = Double(delayLabel.text!) ?? 0.5
+        animationView.animate()
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + animationView.delay + animationView.duration) {
+            let nextAnimation = self.getRandomAnimation()
+            
+            self.presetLabel.text = nextAnimation.animation
+            self.curveLabel.text = nextAnimation.curves
+            self.forceLabel.text = String(format: "%.1f", nextAnimation.force)
+            self.durationLabel.text = String(format: "%.2f", nextAnimation.duration)
+            self.delayLabel.text = String(format: "%.2f", nextAnimation.delay)
+        }
         
     }
     
-    @IBAction func startAnimationButtonPressed(_ sender: UIButton) {
-        
-    }
     
-
+    private func getRandomAnimation() -> Animation {
+        let randomAnimation: Animation
+        let animationPresets = AnimationPreset.allCases.shuffled()
+        let animationCurves = AnimationCurve.allCases.shuffled()
+        
+        randomAnimation = Animation(
+            animation: animationPresets[0].rawValue,
+            curves: animationCurves[0].rawValue,
+            force: Double.random(in: 1..<1.5),
+            duration: Double.random(in: 0..<1.5),
+            delay: Double.random(in: 0..<0.5)
+        )
+        return randomAnimation
+    }
 }
 
