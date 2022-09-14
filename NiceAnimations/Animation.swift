0795364e05cf44c7ed6e942fbab771119ae1e11e
Leo Fridman <madfridman@gmail.com>
2022-09-14 23:30:49 +0200

    Fixes

--- NiceAnimations/Animation.swift
+++ NiceAnimations/Animation.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import SpringAnimation
+
 
 struct Animation {
     let animation: String
@@ -13,4 +15,22 @@ struct Animation {
     let force: Double
     let duration: Double
     let delay: Double
+}
+
+
+extension Animation {
+static func getRandomAnimation() -> Animation {
+    let randomAnimation: Animation
+    let animationPresets = AnimationPreset.allCases.shuffled()
+    let animationCurves = AnimationCurve.allCases.shuffled()
+    
+    randomAnimation = Animation(
+        animation: animationPresets[0].rawValue,
+        curves: animationCurves[0].rawValue,
+        force: Double.random(in: 1..<1.5),
+        duration: Double.random(in: 0..<1.5),
+        delay: Double.random(in: 0..<0.5)
+    )
+    return randomAnimation
+}
 }
--- NiceAnimations/ViewController.swift
+++ NiceAnimations/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SpringAnimation
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet var animationView: SpringView!
     @IBOutlet var presetLabel: UILabel!
@@ -20,7 +20,6 @@ class ViewController: UIViewController {
     
     @IBAction func startAnimationButtonPressed(_ sender: SpringButton) {
         
-        
         animationView.animation = presetLabel.text ?? "pop"
         animationView.curve = curveLabel.text ?? "ease"
         animationView.force = Double(forceLabel.text!) ?? 1.0
@@ -30,7 +29,7 @@ class ViewController: UIViewController {
         
         
         DispatchQueue.main.asyncAfter(deadline: .now() + animationView.delay + animationView.duration) {
-            let nextAnimation = self.getRandomAnimation()
+            let nextAnimation = Animation.getRandomAnimation()
             
             self.presetLabel.text = nextAnimation.animation
             self.curveLabel.text = nextAnimation.curves
@@ -39,22 +38,6 @@ class ViewController: UIViewController {
             self.delayLabel.text = String(format: "%.2f", nextAnimation.delay)
         }
         
-    }
-    
-    
-    private func getRandomAnimation() -> Animation {
-        let randomAnimation: Animation
-        let animationPresets = AnimationPreset.allCases.shuffled()
-        let animationCurves = AnimationCurve.allCases.shuffled()
-        
-        randomAnimation = Animation(
-            animation: animationPresets[0].rawValue,
-            curves: animationCurves[0].rawValue,
-            force: Double.random(in: 1..<1.5),
-            duration: Double.random(in: 0..<1.5),
-            delay: Double.random(in: 0..<0.5)
-        )
-        return randomAnimation
     }
 }
 
